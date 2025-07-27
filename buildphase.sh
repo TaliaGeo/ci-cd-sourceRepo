b0dd4dd26802c54611d62101f47a2046bf0f32dd
TaliaGeo <182074446+TaliaGeo@users.noreply.github.com>
2025-07-27 16:00:59 +0300

    adding versioning to the images

--- buildphase.sh
+++ buildphase.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "starting build phase..."
 IMAGE_NAME="restaurant-inventory-pipeline-image"
-docker build -t "$IMAGE_NAME" .
-echo "Build phase completed successfully."
+VERSION_TAG=$(date +%Y%m%d-%H%M)
+docker build -t "$IMAGE_NAME:$VERSION_TAG" .
+docker tag $IMAGE_NAME:$VERSION $IMAGE_NAME:latest
+echo "Built $IMAGE_NAME with version: $VERSION"
