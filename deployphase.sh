87ed88fb7d51eddb85b2b6aa052e7fd27047a39f
TaliaGeo <182074446+TaliaGeo@users.noreply.github.com>
2025-07-28 09:51:48 +0300

    m

--- deployphase.sh
+++ deployphase.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 CONTAINER_NAME="inventory-container"
 IMAGE_NAME="restaurant-inventory-pipeline-image"  
-VERSION_TAG=$(docker images --format "{{.Tag}}" "$IMAGE_NAME" | grep -v latest | head -n 1)
+
+
 if [ "$(docker ps -a -q -f name=$CONTAINER_NAME)" ]; then
     echo "Stopping and removing existing container..."
     docker stop $CONTAINER_NAME
@@ -10,6 +11,6 @@ else
     echo "No existing container found."
 fi
 
-echo "Deploying container from image version: $VERSION_TAG ..."
-docker run -d --name $CONTAINER_NAME -p 8080:8080 "$IMAGE_NAME:$VERSION_TAG"
+echo "Deploying container from image: $IMAGE_NAME:latest ..."
+docker run -d --name $CONTAINER_NAME -p 8080:8080 "$IMAGE_NAME:latest"
 
