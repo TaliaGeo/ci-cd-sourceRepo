4619f42c0386125946f4b4dd7499d9324c1e7f76
TaliaGeo <182074446+TaliaGeo@users.noreply.github.com>
2025-07-28 13:17:39 +0300

    edit the manage inve

--- cicdpipeline.py
+++ cicdpipeline.py
@@ -24,10 +24,10 @@ while True:
     new_commit = repo.get_branch(BRANCH_NAME).commit.sha
     if new_commit != old_commit:
         print(f"New commit detected: {new_commit}")
-       subprocess.run(["bash", "sourcephase.sh"], check=True)
-       subprocess.run(["bash", "buildphase.sh"] , check=True)
-       subprocess.run(["bash", "deployphase.sh"] , check=True)
-       
+        subprocess.run(["bash", "sourcephase.sh"], check=True)
+        subprocess.run(["bash", "buildphase.sh"] , check=True)
+        subprocess.run(["bash", "deployphase.sh"] , check=True)
+        
         old_commit = new_commit
     else:
         print("No new commits detected.")
