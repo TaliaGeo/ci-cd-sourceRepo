ddf2dd470dd70702214d1ffc08dafc3bc55ce764
TaliaGeo <182074446+TaliaGeo@users.noreply.github.com>
2025-07-28 12:55:41 +0300

    edit the manage inve

--- buildphase.sh
+++ buildphase.sh
@@ -5,3 +5,15 @@ VERSION_TAG=$(date +%Y%m%d-%H%M)
 docker build -t "$IMAGE_NAME:$VERSION_TAG" .
 docker tag $IMAGE_NAME:$VERSION_TAG $IMAGE_NAME:latest
 echo "Built $IMAGE_NAME with version: $VERSION_TAG"
+
+
+if [ "$(docker ps -a -q -f name=$CONTAINER_NAME)" ]; then
+    echo "Stopping and removing existing container..."
+    docker stop $CONTAINER_NAME
+    docker rm $CONTAINER_NAME
+else
+    echo "No existing container found."
+fi
+
+echo "Deploying container from image: $IMAGE_NAME:latest ..."
+docker run -d --name $CONTAINER_NAME -p 8080:8080 "$IMAGE_NAME:latest"
