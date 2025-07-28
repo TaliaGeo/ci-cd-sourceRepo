ac8ab129d69efce0a24fd6b3e3ff23a57b7673a5
TaliaGeo <182074446+TaliaGeo@users.noreply.github.com>
2025-07-28 09:41:32 +0300

    m

--- buildphase.sh
+++ buildphase.sh
@@ -4,4 +4,4 @@ IMAGE_NAME="restaurant-inventory-pipeline-image"
 VERSION_TAG=$(date +%Y%m%d-%H%M)
 docker build -t "$IMAGE_NAME:$VERSION_TAG" .
 docker tag $IMAGE_NAME:$VERSION_TAG $IMAGE_NAME:latest
-echo "Built $IMAGE_NAME with version: $VERSION"
+echo "Built $IMAGE_NAME with version: $VERSION_TAG"
