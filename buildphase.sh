a861582962b06f88b39833740eb7bf3da4e9a6e0
TaliaGeo <182074446+TaliaGeo@users.noreply.github.com>
2025-07-28 09:10:57 +0300

    editting the versioning

--- buildphase.sh
+++ buildphase.sh
@@ -3,5 +3,5 @@ echo "starting build phase..."
 IMAGE_NAME="restaurant-inventory-pipeline-image"
 VERSION_TAG=$(date +%Y%m%d-%H%M)
 docker build -t "$IMAGE_NAME:$VERSION_TAG" .
-docker tag $IMAGE_NAME:$VERSION $IMAGE_NAME:latest
+docker tag $IMAGE_NAME:$VERSION_TAG $IMAGE_NAME:latest
 echo "Built $IMAGE_NAME with version: $VERSION"
